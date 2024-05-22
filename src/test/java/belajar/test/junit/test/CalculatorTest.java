f85441f240b128a72b19106701271e06269f5071
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-22 23:59:08 +0700

    update: Unit Test(Before and After Test)

--- src/test/java/belajar/test/junit/test/CalculatorTest.java
+++ src/test/java/belajar/test/junit/test/CalculatorTest.java
@@ -1,13 +1,32 @@
 package belajar.test.junit.test;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test Calculator class")
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all.");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all.");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before unit test.");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After unit test");
+    }
 
     @Test
     @DisplayName("Test skenario sukses untuk method add(Integer, Integer)")
