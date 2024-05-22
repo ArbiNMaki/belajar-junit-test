365bf7bb812dc6e623be5b049e610b2b138bd7d8
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-22 20:14:25 +0700

    update: Unit Test(JUnit Assertion)

--- src/test/java/belajar/test/junit/test/CalculatorTest.java
+++ src/test/java/belajar/test/junit/test/CalculatorTest.java
@@ -1,6 +1,7 @@
 package belajar.test.junit.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
@@ -8,10 +9,25 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess() {
         var result = calculator.add(10, 20);
+        assertEquals(30, result);
     }
 
     @Test
     public void testRemoveSuccess() {
         var result = calculator.remove(20, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDivideSuccess() {
+        var result = calculator.divide(20, 10);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivideFailed() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           calculator.divide(20, 0);
+        });
     }
 }
