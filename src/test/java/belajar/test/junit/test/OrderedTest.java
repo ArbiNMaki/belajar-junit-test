467f740ef2501c7a40ba205a049d6723d639ead8
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-23 21:10:17 +0700

    update: Unit Test(Lifecycle Test)

--- src/test/java/belajar/test/junit/test/OrderedTest.java
+++ src/test/java/belajar/test/junit/test/OrderedTest.java
@@ -1,21 +1,30 @@
 package belajar.test.junit.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+    private int count = 0;
+
     @Test
     @Order(3)
     public void test3() {
-
+        count++;
+        System.out.println(count);
     }
 
     @Test
     @Order(1)
     public void test1() {
+        count++;
+        System.out.println(count);
+    }
 
+    @Test
+    @Order(2)
+    public void test2() {
+        count++;
+        System.out.println(count);
     }
 }
