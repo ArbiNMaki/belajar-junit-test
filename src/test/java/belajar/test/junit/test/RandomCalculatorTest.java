f31439150cb6b6b84071a460a19503c7a947498d
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-24 10:33:20 +0700

    update: Unit Test(Test With Parameter)

--- src/test/java/belajar/test/junit/test/RandomCalculatorTest.java
+++ src/test/java/belajar/test/junit/test/RandomCalculatorTest.java
@@ -1,11 +1,34 @@
 package belajar.test.junit.test;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 
 public class RandomCalculatorTest extends AbstractCalculatorTest {
+
+    public static List<Integer> parameterSource() {
+        return List.of(1,2,3,4,5);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = {"parameterSource"})
+    public void testWithMethodSource(Integer value) {
+        var result = value + value;
+        Assertions.assertEquals(result, calculator.add(value, value));
+    }
+
+    @DisplayName("Test Calculator with Parameter")
+    @ParameterizedTest(name = "{displayName} with data {0}")
+    @ValueSource(ints = {1,2,3,4,5})
+    public void testWithParameter(int value) {
+        var result = value + value;
+        Assertions.assertEquals(result, calculator.add(value, value));
+    }
 
     @Test
     void testRandom(Random random) {
