0d36792a8c68f77b3038f952a06a69d75cbdde6c
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-24 10:17:14 +0700

    update: Unit Test(Test Loop)

--- src/test/java/belajar/test/junit/test/RandomCalculatorTest.java
+++ src/test/java/belajar/test/junit/test/RandomCalculatorTest.java
@@ -1,7 +1,6 @@
 package belajar.test.junit.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Random;
 
@@ -10,6 +9,22 @@ public class RandomCalculatorTest extends AbstractCalculatorTest {
 
     @Test
     void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a, b);
+        var expect = a + b;
+
+        Assertions.assertEquals(expect, result);
+    }
+
+    @DisplayName("Test Calculator Random")
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeated(Random random, TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + " ke " + repetitionInfo.getCurrentRepetition());
         var a = random.nextInt();
         var b = random.nextInt();
 
