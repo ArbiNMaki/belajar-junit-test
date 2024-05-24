510d00ffbe6046a5d47de94413120122e8bb45ec
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-24 16:36:45 +0700

    update: Unit Testing(Mocking)

---
+++ src/test/java/belajar/test/junit/test/MockTest.java
@@ -0,0 +1,21 @@
+package belajar.test.junit.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class MockTest {
+    @Test
+    void testMock() {
+        List<String> list = Mockito.mock(List.class);
+
+        Mockito.when(list.get(0)).thenReturn("Arbi");
+        Mockito.when(list.get(100)).thenReturn("Kalista");
+
+        System.out.println(list.get(0));
+        System.out.println(list.get(100));
+
+        Mockito.verify(list, Mockito.times(1)).get(0);
+    }
+}
